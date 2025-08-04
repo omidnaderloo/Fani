735253597a1d65b41a5c3c012269b6ca84a9f78f
omidnaderloo <omidnaderloo2@gmail.com>
2025-08-04 12:59:49 +0330

    First
    I dont no

---
+++ 2.py
@@ -0,0 +1,11 @@
+def calc(n1,n2,n3)
+    if n3=="+":
+        print(f"{n1+n2}")
+    if n3=="-":
+        print(f"{n1-n2}")
+    if n3=="*":
+        print(f"{n1*n2}")
+    if n3=="/":
+        print(f"{n1/n2}")
+    else:
+        pass
---
+++ 3.py
@@ -0,0 +1,5 @@
+from 2 import *
+n1=(float(input("adda1")))
+n3=(float(input("caracter")))
+n2=(float(input("adda2")))
+calc(n1,n3,n2)
